3532be28e6b55a671564fdf008924306179f1a74
Yuiji Imagawa <using923@gmail.com>
2014-07-06 15:20:01 +0900

    Resolve kadai4

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -9,23 +11,30 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Main {
-    public static void main(String[] args) {
-        JSONParser parser = new JSONParser();
 
+    public static void main(String[] args) throws FileNotFoundException {
+        JSONParser parser = new JSONParser();
+        File CookData =
+                new File("/Users/Yuji/kentya6-sezemi-2014-readable-code-2/CookData.json");
+        FileReader CookDataReader = new FileReader(CookData);
+        ArrayList<String> recipeList = new ArrayList<>();
         try {
-            Object parseObject = parser.parse(new FileReader("CookData.json"));
+            Object parseObject = parser.parse(CookDataReader);
             JSONObject jsonObject = (JSONObject) parseObject;
-            String recipeName = (String) jsonObject.get("RecipeName");
-            System.out.println(recipeName);
+            recipeList = (ArrayList<String>) jsonObject.get("RecipeName");
+            
+            for (int i = 0; i < recipeList.size(); i++) {
+                System.out.println(recipeList.get(i));
+            }
         } catch (FileNotFoundException notfound) {
-            System.out.println("ファイルが見つかりません");
+            System.out.println("ファイルが見つかりませんでした");
             notfound.printStackTrace();
         } catch (IOException ioe) {
-            System.out.println("IO error");
+            System.out.println("入出力エラーです");
             ioe.printStackTrace();
-        } catch (ParseException e) {
-            System.out.println("parse error");
-            parseException.printStackTrace();
+        } catch (ParseException pe) {
+            System.out.println("JSONが間違っています");
+            pe.printStackTrace();
         }
     }
 }
