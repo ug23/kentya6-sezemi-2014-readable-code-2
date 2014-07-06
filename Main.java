fb22999d023b55690f48d20c38bc73e6c263ff4a
Kengo Yokoyama <s1420714@u.tsukuba.ac.jp>
2014-07-06 11:50:51 +0900

    spec3 :レシピデータを別ファイル（CookData.json）に分離

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("オムライス");
+		JSONParser parser = new JSONParser();
+ 
+		try {	 
+			Object parseObject = parser.parse(new FileReader("CookData.json"));
+			JSONObject jsonObject = (JSONObject)parseObject;
+			String recipeName = (String)jsonObject.get("RecipeName");
+			System.out.println(recipeName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
