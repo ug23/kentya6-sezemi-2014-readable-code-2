d314b243a53700c755954a685f4b0cff943498fc
Yuiji Imagawa <using923@gmail.com>
2014-07-06 14:27:54 +0900

    例外のローカル変数名を変更

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,20 +9,23 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Main {
-	public static void main(String[] args) {
-		JSONParser parser = new JSONParser();
- 
-		try {	 
-			Object parseObject = parser.parse(new FileReader("CookData.json"));
-			JSONObject jsonObject = (JSONObject)parseObject;
-			String recipeName = (String)jsonObject.get("RecipeName");
-			System.out.println(recipeName);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+
+        try {
+            Object parseObject = parser.parse(new FileReader("CookData.json"));
+            JSONObject jsonObject = (JSONObject) parseObject;
+            String recipeName = (String) jsonObject.get("RecipeName");
+            System.out.println(recipeName);
+        } catch (FileNotFoundException notfound) {
+            System.out.println("ファイルが見つかりません");
+            notfound.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("IO error");
+            ioe.printStackTrace();
+        } catch (ParseException e) {
+            System.out.println("parse error");
+            parseException.printStackTrace();
+        }
+    }
 }
